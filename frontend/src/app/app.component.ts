2b2493119fbe0647f42ae4198a229c72d62f5b3c
michel sampil <michel.sampil@gmail.com>
2023-11-06 22:57:46 -0300

    basic web socket

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { SocketService } from './socket.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  message = '';
+
+  constructor(private socketService: SocketService) {}
+
+  ngOnInit() {
+    this.socketService.getNewMessage().subscribe((message: string) => {
+      console.log(message);
+      this.message = message;
+    });
+  }
+}
